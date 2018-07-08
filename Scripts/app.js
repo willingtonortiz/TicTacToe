bd206719c9ed010dfe5d172460cd6c636797b360
Willington <willingtonortizmaurtua@hotmail.com>
2018-07-08 18:07:57 -0500

    funcionalidad para contar puntajes

---
+++ Scripts/app.js
@@ -0,0 +1,98 @@
+/* DEFINICIÓN DE CLASES */
+
+class CJugador {
+    constructor(nombre) {
+        this.Nombre = nombre;
+        this.Puntaje = 0;
+    }
+}
+
+class CCoordenada {
+
+    constructor(x = 0, y = 0) {
+        this.X = x;
+        this.Y = y;
+        this.Simbolo = 'X';
+        this.Libre = true;
+    }
+
+}
+
+class CJuego {
+    constructor(filas = 3, columnas = 3) {
+        this.Filas = filas;
+        this.Columnas = columnas;
+        this.InicializarMatriz();
+    }
+
+    //Creación de objetos en la matriz de juego
+    InicializarMatriz() {
+        this.Tablero = new Array(this.Filas);
+        for (let i = 0; i < this.Filas; ++i) {
+            this.Tablero[i] = new Array(this.Columnas);
+            for (let j = 0; j < this.Columnas; ++j) {
+                this.Tablero[i][j] = new CCoordenada(i, j);
+            }
+        }
+        return this;
+    }
+
+    ImprimirTableroConsola() {
+        for (let i = 0; i < this.Filas; ++i) {
+            let cadena = '';
+            for (let j = 0; j < this.Columnas; ++j) {
+                cadena += this.Tablero[i][j].Simbolo + '\t';
+            }
+            console.log(cadena);
+        }
+        return this;
+    }
+
+    ContarPuntaje() {
+        let puntaje1 = 0,
+            contador1 = 0;
+        let puntaje2 = 0,
+            contador2 = 0;
+
+        //Conteo horizontal
+        for (let i = 0; i < this.Filas; ++i) {
+            for (let j = 0; j < this.Columnas; ++j) {
+                if (this.Tablero[i][j].Simbolo === 'X') {
+                    contador1++;
+                    contador2 = 0;
+                } else {
+                    contador2++;
+                    contador1 = 0;
+                }
+                if (contador1 >= 3) puntaje1++;
+                if (contador2 >= 3) puntaje2++;
+            }
+            contador1 = contador2 = 0;
+        }
+
+        //Conteo vertical
+        for (let j = 0; j < this.Columnas; ++j) {
+            for (let i = 0; i < this.Filas; ++i) {
+                if (this.Tablero[i][j].Simbolo === 'X') {
+                    contador1++;
+                    contador2 = 0;
+                } else {
+                    contador2++;
+                    contador1 = 0;
+                }
+                if (contador1 >= 3) puntaje1++;
+                if (contador2 >= 3) puntaje2++;
+            }
+            contador1 = contador2 = 0;
+        }
+
+        //Conteo diagonal
+
+        console.log(puntaje1, puntaje2);
+        return this;
+    }
+}
+
+let juego = new CJuego(5, 5);
+
+juego.ImprimirTableroConsola().ContarPuntaje();
