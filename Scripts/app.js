a4f8b0e7b45e2482b101be0e90d9edefda5022bc
Joaquin08 <jgarciag8274@gmail.com>
2018-07-12 19:51:36 -0500

    se agrego las prioridades a la ia

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,12 +5,13 @@ class CJugador {
         this.Puntaje = 0;
         this.Simbolo = simbolo;
         this.Jugada = null;
+        
     }
 }
-class Par{
-    constructor(valor1,valor2){
-        this.first=valor1;
-        this.second=valor2;
+class Par {
+    constructor(valor1, valor2) {
+        this.first = valor1;
+        this.second = valor2;
     }
 }
 class CIA extends CJugador {
@@ -18,9 +19,9 @@ class CIA extends CJugador {
     PensarJugada(jugada, limitex, limitey, Tablero) {
         for (let y = jugada.Y - 2; y <= jugada.Y + 2; y++) {
             for (let x = jugada.X - 2; x <= jugada.X + 2; x++) {
-                if (y < 0 || y >= limitey) 
+                if (y < 0 || y >= limitey)
                     break;
-                if (x < 0 || x >= limitex) 
+                if (x < 0 || x >= limitex)
                     continue;
                 if (Tablero[y][x].Simbolo == jugada.Simbolo && Tablero[y][x] != jugada) {
                     if (this.EvaluarJugada(x, y, jugada, Tablero, limitex, limitey)) {
@@ -34,81 +35,93 @@ class CIA extends CJugador {
 
     //Funcion booleana
     EvaluarJugada(x, y, jugada, Tablero, limitex, limitey) {
-        if ((y - jugada.Y == 2 || y - jugada.Y == -2) && (x - jugada.X == 1 || x - jugada.X == -1)) 
+        if ((y - jugada.Y == 2 || y - jugada.Y == -2) && (x - jugada.X == 1 || x - jugada.X == -1))
             return false;
-        if ((x - jugada.X == 2 || x - jugada.X == -2) && (y - jugada.Y == 1 || y - jugada.Y == -1)) 
+        if ((x - jugada.X == 2 || x - jugada.X == -2) && (y - jugada.Y == 1 || y - jugada.Y == -1))
             return false;
         let dy, dx;
-        if (y > jugada.Y) 
+        if (y > jugada.Y)
             dy = -1;
         else {
-            if (y == jugada.Y) 
+            if (y == jugada.Y)
                 dy = 0;
-            else 
+            else
                 dy = +1;
         }
-        if (x > jugada.X) 
+        if (x > jugada.X)
             dx = -1;
         else {
-            if (x == jugada.X) 
+            if (x == jugada.X)
                 dx = 0;
-            else 
+            else
                 dx = +1;
         }
-        let choque = true;
-        while (true) {
-            if (y + dy < 0 || y + dy >= limitey || x + dx < 0 || x + dx >= limitex) {
-                dy *= -1;
-                dx *= -1;
-                if (choque == true) {
-                    choque = false;
-                } else {
-                    break;
-                }
-            }
-            if (Tablero[y + dy][x + dx].Simbolo == '') {
-                let posiblejugada = new CCoordenada(x+dx,y+dy);
-                posiblejugada.Simbolo=this.Simbolo;
-                //Tablero[y + dy][x + dx].Simbolo = this.Simbolo;
-                //this.Jugada = Tablero[y + dy][x + dx];
-                if(choque==true)
-
-                if(choque==false)
-                this.posiblesJugadas.push();
-            } else {
-                if (Tablero[y + dy][x + dx].Simbolo === this.Simbolo) {
-                    x += dx;
-                    y += dy;
-                    dy *= -1;
-                    dx *= -1;
-
-                    if (choque == true)
-                        choque = false; 
-                    else {
-                        break;
-                    }
-                }
-            }
-            x += dx;
-            y += dy;
-        }
+      this.EvaluarPrioridad(dx,dy,x,y,jugada,Tablero);
     }
-
+    EvaluarPrioridad(dx, dy, x, y, jugada, Tablero, salto = true,agregarJugadas=true) {
+        let prioridad = 0;
+        let limites = 0;
+        while (limites < 2) {
+            if (!(y + dy < Tablero.length && y + dy >= 0 && x + dx >= 0 && x + dx < Tablero[0].length) || Tablero[y + dy][x + dx] == this.Simbolo)
+                limites++;
+            else if (Tablero[y + dy][x + dx].Simbolo == '') {
+                if (salto  )
+                    if(y + 2 * dy < Tablero.length && y + 2 * dy >= 0 && x + 2 * dx >= 0 && x + 2 * dx < Tablero[0].length)
+                    if(Tablero[y + 2 * dy][x + 2 * dx].Simbolo == jugada.Simbolo)
+                        prioridad += this.EvaluarPrioridad(dx, dy, x + 2 * dx, y + 2 * dy, jugada, Tablero, false,false);
+                    if(limites==0 && agregarJugadas ){
+                        let posiblejugada = new CCoordenada(x + dx, y + dy);
+                        posiblejugada.Simbolo = this.Simbolo;
+                        let PJ=new Par(prioridad,posiblejugada);
+                        this.PosiblesJugadas.push(PJ);
+                    }
+                salto=false;
+                limites++;
+            }
+            x+=dx;
+            y+=dy;
+            if (limites == 1) {
+                if(prioridad==0){
+                dx *= -1;
+                dy *= -1;
+                }
+                prioridad++;
+            }
+        }
+        if(agregarJugadas==false)
+        return prioridad;
+    }
+    BuscarJugadaOptima(Tablero) {
+        let mayor = 0;
+        for (let i = 1; i < this.PosiblesJugadas.length; ++i) {
+            if (this.PosiblesJugadas[mayor].first <= this.PosiblesJugadas[i].first)
+                mayor = i;
+        }
+        Tablero[this.PosiblesJugadas[mayor].second.Y][this.PosiblesJugadas[mayor].second.X].Simbolo = this.Simbolo;
+        this.Jugada = Tablero[this.PosiblesJugadas[mayor].second.Y][this.PosiblesJugadas[mayor].second.X];
+    }
     //Funcion void
     jugarIA(jugada, limitex, limitey, Tablero) {
-       /* if (!this.PensarJugada(jugada, limitex, limitey, Tablero)) {
-            if (this.jugada != null) {
-                if (!this.PensarJugada(this.jugada, limitex, limitey, Tablero)) {
-                    //this.jugada.Simbolo = '';
-                    let jugadaNueva = new CCoordenada(this.jugada.X, this.jugada.Y);
-                    this.PensarJugada(jugadaNueva, limitex, limitey, Tablero);
-                }
-            } else {
-                this.JugadaRandom(Tablero);
-            }
-        } */
-        let posiblesJugadas = new Array;
-        
+
+       this.PosiblesJugadas = new Array(0);
+        //Se analiza las posibles jugadas de defensa que se puede realizar
+        this.PensarJugada(jugada, limitex, limitey, Tablero);
+        if (this.Jugada != null) {
+            //Se analiza las posibles jugadas de ataque que se pueden realizar
+            this.PensarJugada(this.Jugada, limitex, limitey, Tablero)
+            //Si no se puede realizar ninguna jugada de defensa ni ataque, se busca generar una jugada de ataque
+           /* if (this.PosiblesJugadas.length == 0) {
+                let jugadaNueva = new CCoordenada(this.Jugada.X, this.Jugada.Y);
+                this.PensarJugada(jugadaNueva, limitex, limitey, Tablero);
+            }*/
+        }
+        //Se selecciona la jugada con
+        //Si no se puede generar ninguna jugada, se juega al azar
+        if (this.PosiblesJugadas.length == 0)
+            this.JugadaRandom(Tablero);
+        else
+            //Se selecciona la jugada con  mayor prioridad
+            this.BuscarJugadaOptima(Tablero);
     }
 
     JugadaRandom(Tablero) {
